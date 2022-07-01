0040671fc65ee188cbfbbd0d0530cdbe45428a3f
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-01 03:15:46 -0500

    js file update

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,14 @@ var cityInfo = document.querySelector("div");
 
 var getCity = function(city) {
     // insert API url
-    var requestUrl = "api.openweathermap.org/data/2.5/weather?id=524901&appid=20c7d9be36c3304993a423c8f846f4cf";
+    var requestUrl = "api.openweathermap.org/data/2.5/weather?id=524901&appid=";
 
     fetch(requestUrl).then(function (response) {
         // if successful
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
-            })
+            });3
         }
     })
 };
