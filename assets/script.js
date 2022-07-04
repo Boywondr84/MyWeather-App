e09ee0b3010b204de025696140b2c454d3b04736
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-04 01:25:53 -0500

    TODO get lat & lon from data and pass through 2nd api fetch

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 
 
-// get API
+// get Geocoding API
 
 var getCity = function(city) {
     // insert API url
@@ -10,14 +10,35 @@ var getCity = function(city) {
         // if successful
         if (response.ok) {
             response.json().then(function (data) {
-                console.log("working?");
-            })
-        } else {
+                console.log(data);
+        //         for (var i = 0; i < data.length; i++) {
+        //             if (data[0].lat) {
+        //                 lat = data[0].lat;
+        //             }
+        //         }
+        //         var lat = data[0].lat;
+        //         var lon = data[0].lon;
+        //         retainCityName (lat, lon);
+        //     })
+        // }
+        else {
             alert("Error. City not found.");
         }
     });
-};
-
+}       
+        
+// get lat/lon API
+var retainCityName = function(lat, lon) {
+    // insert API url
+    var cityLatLonUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&appid=386d421121bbbad42dc1ad82319e7fc0";
+    fetch(cityLatLonUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            })
+        }
+    });
+}
 // getCity("Albany");
 
 // Form info capture
@@ -37,5 +58,5 @@ if (cityName) {
     alert("Please enter a city name.");
 }
     // console.log(event);
-};
+}
 citySearchEl.addEventListener("submit", formSubmitHandler);
