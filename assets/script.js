bedb5ca590a423545ec4d48a9e1db7ad15f33afb
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-04 02:12:46 -0500

    got lat lon data from first api and successfully passed it into 2nd api

--- assets/script.js
+++ assets/script.js
@@ -4,23 +4,29 @@
 
 var getCity = function(city) {
     // insert API url
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=386d421121bbbad42dc1ad82319e7fc0";
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=386d421121bbbad42dc1ad82319e7fc0";
     console.log("testing");
     fetch(requestUrl).then(function (response) {
         // if successful
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
-        //         for (var i = 0; i < data.length; i++) {
-        //             if (data[0].lat) {
-        //                 lat = data[0].lat;
-        //             }
-        //         }
-        //         var lat = data[0].lat;
-        //         var lon = data[0].lon;
-        //         retainCityName (lat, lon);
-        //     })
-        // }
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].lat) {
+                        lat = data[i].lat;
+                        console.log(lat);
+                    }
+                    if (data[i].lon) {
+                        lon = data[i].lon;
+                        console.log(lon);
+                    }
+                }
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+                getCityNameWithLatLon (lat, lon);
+                
+    })
+}
         else {
             alert("Error. City not found.");
         }
@@ -28,7 +34,7 @@ var getCity = function(city) {
 }       
         
 // get lat/lon API
-var retainCityName = function(lat, lon) {
+var getCityNameWithLatLon = function(lat, lon) {
     // insert API url
     var cityLatLonUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&appid=386d421121bbbad42dc1ad82319e7fc0";
     fetch(cityLatLonUrl).then(function (response) {
